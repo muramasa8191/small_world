18ab147e7b0c25a3205b0e4ab6ee7ad52f410a4b
Masa Tamura <muramasa8191@gmail.com>
2022-03-14 17:16:51 +0900

    Add comment

---
+++ lib/small_world/blogs/comment.ex
@@ -0,0 +1,20 @@
+defmodule SmallWorld.Blogs.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias SmallWorld.Blogs.Article
+
+  schema "comments" do
+    field :body, :string
+    belongs_to :article, Article
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:body, :article_id])
+    |> validate_required([:body, :article_id])
+  end
+end
